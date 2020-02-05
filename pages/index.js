ae3f2bc995a765013089ce21169069538433d609
Jhonatan I <jhonatan-nascimento94@hotmail.com>
2020-02-05 11:05:31 -0300

    Add hocs

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import Link from 'next/link';
 import Head from 'next/head';
+import withAnalytics from '../src/hocs/withAnalytics';
 
 const Home = () => (
     <div>
@@ -15,4 +16,4 @@ const Home = () => (
     </div>
 );
 
-export default Home;
+export default withAnalytics()(Home);
