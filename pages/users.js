63525416a60ff385c603438bb9a4685aea92436c
Jhonatan I <jhonatan-nascimento94@hotmail.com>
2020-02-04 21:38:25 -0300

    Index and User page

---
+++ pages/users.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import axios from 'axios';
+
+import Link from 'next/link';
+import Head from 'next/head';
+
+const User = ({ users }) => (
+    <div>
+      <Head>
+        <title>Usuários</title>
+      </Head>
+      <ul>
+        { users.map(user => (
+          <li key={user.id}>{user.login}</li>
+        ))}
+        <Link href="/">
+          <a>Voltar</a>
+        </Link>
+      </ul>
+    </div>
+);
+
+User.getInitialProps = async () => {
+  const response = await axios.get(
+      'https://api.github.com/orgs/rocketseat/members'
+  );
+
+  return { users: response.data };
+};
+
+export default User;
